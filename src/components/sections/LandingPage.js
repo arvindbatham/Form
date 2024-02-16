54f55920fe46a03eda2fc1538639fc132ea2ba24
arvindbatham <144878397+arvindbatham@users.noreply.github.com>
2024-02-16 15:14:39 +0530

    landing page done

--- src/components/sections/LandingPage.js
+++ src/components/sections/LandingPage.js
@@ -1,10 +1,28 @@
 import React from "react";
-
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
 function LandingPage() {
   return (
     <div id="landingPage">
       <div className="grid-container">
-        <div className="left">hii</div>
+        <div className="left">
+          <div className="box">
+            <div className="main-heading">REMOTE WORK LOCATION DECLARATION</div>
+            <div className="second">
+              <button>Start</button>
+              <span>
+                press <stron>Enter ↵</stron>
+              </span>
+            </div>
+            <div className="third">
+              <AccessTimeIcon
+                style={{
+                  fontSize: "19px",
+                }}
+              />
+              <span> Takes 1 minute 30 seconds </span>
+            </div>
+          </div>
+        </div>
         <div className="right"></div>
       </div>
     </div>
