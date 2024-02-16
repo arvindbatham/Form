cc1edfe068b26db5a2f7054de548288a852ba171
arvindbatham <144878397+arvindbatham@users.noreply.github.com>
2024-02-16 14:05:24 +0530

    project setup

---
+++ src/components/sections/LandingPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div id="landingPage">
+      <div className="grid-container">
+        <div className="left">hii</div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
